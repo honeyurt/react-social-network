532d421b8abfdd0e265ca71e2b084df958a796ce
Alexander <honeyurt13@gmail.com>
2021-09-10 16:02:01 +0300

    header ui update

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -8,9 +8,9 @@ import { Link } from 'react-router-dom';
 import logoHeader from '../../assets/img/header-logo.png';
 import styles from './Header.module.css';
 
-const Header: React.FC = (props) => {
+const Header = () => {
   const dispatch = useDispatch();
-  const { isAuth } = useSelector((state: RootState) => state.auth);
+  const { isAuth, login } = useSelector((state: RootState) => state.auth);
   const history = useHistory();
 
   const onLogoutHandler = () => {
@@ -29,9 +29,23 @@ const Header: React.FC = (props) => {
         </Link>
       </div>
       {isAuth ? (
-        <p className={styles.header__link} onClick={onLogoutHandler}>
-          Logout
-        </p>
+        <div className={styles.header__link}>
+          <nav>Hello, {login}</nav>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            fill-rule="nonzero"
+            onClick={onLogoutHandler}>
+            <path
+              d="M10 2v2h12v16h-12v2h14v-20h-14zm0 7.408l2.963 2.592-2.963 2.592v-1.592h-8v-2h8v-1.592zm-2-4.408v4h-8v6h8v4l8-7-8-7z"
+              fill="currentColor"
+              fill-rule="nonzero"
+            />
+          </svg>
+        </div>
       ) : (
         <p className={styles.header__link} onClick={onLoginHandler}>
           Login
