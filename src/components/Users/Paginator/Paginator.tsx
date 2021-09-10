6a5107ee10ab506f9b1a18da20f8a9d95fa59803
Alexander <honeyurt13@gmail.com>
2021-09-10 11:35:26 +0300

    paginator fix

--- src/components/Users/Paginator/Paginator.tsx
+++ src/components/Users/Paginator/Paginator.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import Button from '../../../UI/Button/Button';
 import styles from './Paginator.module.css';
 
 interface Props {
@@ -7,6 +8,7 @@ interface Props {
   pageSize: number;
   currentPage: number;
   onPageClickHandler: (page: number) => void;
+  portionSize?: number;
 }
 
 const Paginator: React.FC<Props> = ({
@@ -14,24 +16,44 @@ const Paginator: React.FC<Props> = ({
   totalUsersCount,
   currentPage,
   onPageClickHandler,
+  portionSize = 10,
 }) => {
-  const pagesCount = Math.ceil(totalUsersCount / pageSize) / 100; // FIXME
+  const pagesCount = Math.ceil(totalUsersCount / pageSize);
   const pages: number[] = [];
 
   for (let i = 1; i < pagesCount; i++) {
     pages.push(i);
   }
 
+  const portionCount = Math.ceil(pagesCount / portionSize);
+  const [portionNumber, setPortionNumber] = React.useState<number>(1);
+
+  const leftNumber = (portionNumber - 1) * portionSize + 1;
+  const rightNumber = portionNumber * portionSize;
+
+  React.useEffect(() => {
+    setPortionNumber(Math.ceil(currentPage / portionSize));
+  }, [currentPage, portionSize]);
+
   return (
     <div className={styles.pagination}>
-      {pages.map((page) => (
-        <span
-          key={page}
-          className={currentPage === page ? styles.selected : styles.notSelected}
-          onClick={onPageClickHandler.bind(null, page)}>
-          {page}
-        </span>
-      ))}
+      {portionNumber > 1 && (
+        <Button onClick={() => setPortionNumber(portionNumber - 1)}>{'<<<'}</Button>
+      )}
+
+      {pages
+        .filter((page) => page >= leftNumber && page <= rightNumber)
+        .map((page) => (
+          <span
+            key={page}
+            className={currentPage === page ? styles.selected : styles.notSelected}
+            onClick={onPageClickHandler.bind(null, page)}>
+            {page}
+          </span>
+        ))}
+      {portionCount > portionNumber && (
+        <Button onClick={() => setPortionNumber(portionNumber + 1)}>{'>>>'}</Button>
+      )}
     </div>
   );
 };
