e68efca885e8f7266eeedd031171e443b29103be
Alexander <honeyurt13@gmail.com>
2021-09-10 16:04:38 +0300

    warning fix

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -36,13 +36,11 @@ const Header = () => {
             width="24"
             height="24"
             viewBox="0 0 24 24"
-            fill="currentColor"
-            fill-rule="nonzero"
             onClick={onLogoutHandler}>
             <path
               d="M10 2v2h12v16h-12v2h14v-20h-14zm0 7.408l2.963 2.592-2.963 2.592v-1.592h-8v-2h8v-1.592zm-2-4.408v4h-8v6h8v4l8-7-8-7z"
               fill="currentColor"
-              fill-rule="nonzero"
+              fillRule="nonzero"
             />
           </svg>
         </div>
