2d881bed146ea40f6f489bf3c95deeee5ef47904
Alexander <honeyurt13@gmail.com>
2022-07-03 00:18:26 +0300

    remove extra code

--- src/redux/reducers/index.ts
+++ src/redux/reducers/index.ts
@@ -1,12 +1,10 @@
 import { combineReducers } from 'redux';
 import { usersReducer } from './users';
-import { profileReducer } from './profile';
 import { dialogsReducer } from './dialogs';
 import { chatReducer } from './chat';
 
 export const rootReducer = combineReducers({
   users: usersReducer,
-  profile: profileReducer,
   dialogs: dialogsReducer,
   chat: chatReducer,
 });
