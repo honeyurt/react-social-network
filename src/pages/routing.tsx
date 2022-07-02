adec0129157860d78757726932b3758d92fbca48
Alexander <honeyurt13@gmail.com>
2022-07-02 20:47:00 +0300

    move profile logic to mobx

--- src/pages/routing.tsx
+++ src/pages/routing.tsx
@@ -3,19 +3,19 @@ import { Switch, Route } from 'react-router-dom';
 import Chat from '../components/Chat/Chat';
 import Dialogs from '../components/Messenger/Dialogs';
 import StartMessaging from '../components/Messenger/Messages/StartMessaging/StartMessaging';
-import Profile from '../components/Profile/Profile';
 import Settings from '../components/Settings/Settings';
 import Users from '../components/Users/Users';
 import { HomePage } from './home';
 import { LoginPage } from './login';
 import { NewsPage } from './news';
+import { ProfilePage } from './profile';
 import { NotFound } from './not-found';
-import { ROOT_ROUTE, NEWS_ROUTE, LOGIN_ROUTE } from './routes';
+import { ROOT_ROUTE, NEWS_ROUTE, LOGIN_ROUTE, PROFILE_ROUTE } from './routes';
 
 export const PagesRouter = () => (
   <Switch>
     <Route exact path={ROOT_ROUTE} component={HomePage} />
-    <Route exact path="/profile/:userId?" component={Profile} />
+    <Route exact path={`${PROFILE_ROUTE}/:userId?`} component={ProfilePage} />
     <Route exact path={NEWS_ROUTE} component={NewsPage} />
     <Route exact path="/dialogs/:userId?/:messages?" component={Dialogs} />
     <Route exact path="/dialog-create" component={StartMessaging} />
